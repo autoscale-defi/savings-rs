dfe7d64af4113cbecd191702d6f7d06ee55f236f
Fusee <quentus123@orange.fr>
2023-10-06 21:20:57 +0200

    initialized workspace

--- platforms/ashswap/src/empty.rs
+++ platforms/ashswap/src/empty.rs
@@ -4,7 +4,4 @@ multiversx_sc::imports!();
 
 /// An empty contract. To be used as a template when starting a new contract from scratch.
 #[multiversx_sc::contract]
-pub trait SavingRsContract {
-    #[init]
-    fn init(&self) {}
-}
+pub trait AshSwapPlatformContract {}

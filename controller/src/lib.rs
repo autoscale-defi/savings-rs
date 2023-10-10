f18bc2d50be3faeaa6387acefeba4a750bfa53d6
SkyzoxRobin <robinskyzox@gmail.com>
2023-10-10 22:24:50 +0200

    draft deposit & merge savings tokens

--- controller/src/lib.rs
+++ controller/src/lib.rs
@@ -1,17 +1,88 @@
 #![no_std]
 
+use token::SavingsTokenAttributes;
+
 multiversx_sc::imports!();
 
+pub mod token;
 
 #[multiversx_sc::contract]
-pub trait ControllerContract {
+pub trait ControllerContract: token::TokenModule {
     #[init]
-    fn init(&self) {}
+    fn init(&self, usdc_token_id: TokenIdentifier) {
+        self.usdc_token().set_if_empty(usdc_token_id);
+    }
 
     // ROBIN
     #[payable("*")]
     #[endpoint]
-    fn deposit(&self) {}
+    fn deposit(&self) -> EsdtTokenPayment<Self::Api> {
+        let payments = self.call_value().all_esdt_transfers();
+        let usdc_payment = payments
+            .try_get(0)
+            .unwrap_or_else(|| sc_panic!("empty payments"));
+        let additional_payments = payments.slice(1, payments.len()).unwrap_or_default();
+
+        self.usdc_token()
+            .require_same_token(&usdc_payment.token_identifier);
+        self.savings_token()
+            .require_all_same_token(&additional_payments);
+
+        let last_bloc = self.blockchain().get_block_nonce();
+        let attributes = SavingsTokenAttributes {
+            reward_per_share: self.reward_per_share().get(),
+            accumulated_rewards: BigUint::zero(),
+            last_bloc,
+        };
+
+        let caller = self.blockchain().get_caller();
+        let new_savings_token = self.create_savings_token_by_merging(
+            usdc_payment.amount,
+            &attributes,
+            &additional_payments,
+        );
+
+        self.send()
+            .direct_non_zero_esdt_payment(&caller, &new_savings_token);
+
+        new_savings_token
+    }
+
+    fn create_savings_token_by_merging(
+        &self,
+        amount: BigUint,
+        _attributes: &SavingsTokenAttributes<Self::Api>,
+        payments: &ManagedVec<EsdtTokenPayment<Self::Api>>,
+    ) -> EsdtTokenPayment<Self::Api> {
+        // merge les attributs
+        let merged_attributes = SavingsTokenAttributes {
+            reward_per_share: BigUint::zero(), // todo
+            accumulated_rewards: BigUint::zero(), //todo
+            last_bloc: 0,                      // todo
+        };
+
+        // additionner la nouvelle position + les anciennes 
+        // burn les anciennes positions 
+        // baisser la supply total des savings token 
+        // est-ce que je mettrai pas un IF pour la loop et je rentre dedans que si j'ai besoin de merge ? si la len des paiements est de 0 je rentre pas
+        let mut new_amount = amount;
+        for payment in payments.iter() {
+            new_amount += payment.amount.clone();
+
+            self.savings_token().nft_burn(payment.token_nonce, &payment.amount);
+            self.savings_token_supply().update(|x| *x -= payment.amount);
+        }
+
+        // creer le nouveau savings token
+        let new_savings_token = self
+            .savings_token()
+            .nft_create(new_amount.clone(), &merged_attributes);
+
+        self.savings_token_supply().update(|x| *x += new_amount);
+        // je dois merge seulement si il y a des payments - pas de merge sinon
+
+        new_savings_token
+    }
 
     // ROBIN
     #[payable("*")]
@@ -39,19 +110,13 @@ pub trait ControllerContract {
     #[only_owner]
     #[endpoint(addPlatform)]
     fn add_platform(&self) {}
-    
+
     // NICOLAS
     #[only_owner]
     #[endpoint(setPlatformDistribution)]
     fn set_platforms_distribution(&self) {
-        // quand on change la répartition alors on va withdraw + redeposit all dans cette fonction 
+        // quand on change la répartition alors on va withdraw + redeposit all dans cette fonction
     }
-
-    // ROBIN
-    #[only_owner]
-    #[payable("EGLD")]
-    #[endpoint(issueAndSetLocalRoles)]
-    fn issue_and_set_local_roles(&self) {}
 
     // ROBIN
     fn merge_position(&self) {}
@@ -69,5 +134,18 @@ pub trait ControllerContract {
     // DUOQ
     #[only_owner]
     #[endpoint(setRewardsPerShare)]
-    fn set_rewards_per_share(&self) {}
+    fn set_reward_per_share(&self) {
+
+        // est-ce qu'on a besoin du savings_token_supply pour le calculer ? 
+    }
+
+    #[view(getSavingsTokenSupply)]
+    #[storage_mapper("savingsTokenSupply")]
+    fn savings_token_supply(&self) -> SingleValueMapper<BigUint>;
+
+    #[storage_mapper("rewardPerShare")]
+    fn reward_per_share(&self) -> SingleValueMapper<BigUint>;
+
+    #[storage_mapper("usdcTokenId")]
+    fn usdc_token(&self) -> FungibleTokenMapper<Self::Api>;
 }
---
+++ controller/src/token.rs
@@ -0,0 +1,35 @@
+multiversx_sc::imports!();
+multiversx_sc::derive_imports!();
+
+#[derive(TypeAbi, TopEncode, TopDecode)]
+pub struct SavingsTokenAttributes<M: ManagedTypeApi> {
+    pub reward_per_share: BigUint<M>,
+    pub accumulated_rewards: BigUint<M>,
+    pub last_bloc: u64
+}
+
+#[multiversx_sc::module]
+pub trait TokenModule {
+    #[only_owner]
+    #[payable("EGLD")]
+    #[endpoint(registerSavingsToken)]
+    fn register_savings_token(
+        &self,
+        token_display_name: ManagedBuffer,
+        token_ticker: ManagedBuffer,
+        num_decimals: usize,
+    ) {
+        let issue_cost = self.call_value().egld_value();
+        self.savings_token().issue_and_set_all_roles(
+            EsdtTokenType::Meta,
+            issue_cost.clone_value(),
+            token_display_name,
+            token_ticker,
+            num_decimals,
+            None,
+        );
+    }
+
+    #[storage_mapper("savingsTokenId")]
+    fn savings_token(&self) -> NonFungibleTokenMapper<Self::Api>;
+}

08eab7a7b2361b2640916253bdec5013247c3144
Nicolas Vergez - PhyloFane <nicolas.vergez@icloud.com>
2023-10-17 11:49:58 +0200

    update script

--- interaction/aprCalculator.py
+++ interaction/aprCalculator.py
@@ -2,6 +2,8 @@ from rich import print
 from rich.panel import Panel
 from rich.prompt import Prompt
 
+USDC_TO_SHARES = 1000000  # Conversion of 1 USDC to shares
+
 def apr_to_rewards_per_block(apr: float) -> float:
     blocks_per_day = (60 * 60 * 24) / 6
     daily_reward_per_share = apr / 365
@@ -14,7 +16,8 @@ def rewards_per_block_to_apr(rewards_per_block: float) -> float:
     apr = daily_reward_per_share * 365
     return apr
 
-def calculate_rewards(duration_in_days: float, apr: float, number_of_shares: float) -> float:
+def calculate_rewards(duration_in_days: float, apr: float, amount_in_usdc: float) -> float:
+    number_of_shares = amount_in_usdc * USDC_TO_SHARES
     rewards_per_block = apr_to_rewards_per_block(apr)
     blocks_per_day = (60 * 60 * 24) / 6
     daily_reward = rewards_per_block * blocks_per_day * number_of_shares
@@ -22,32 +25,35 @@ def calculate_rewards(duration_in_days: float, apr: float, number_of_shares: flo
     return total_reward_for_duration
 
 if __name__ == "__main__":
-    print(Panel("[bold green]Outil de Conversion[/bold green]"))
+    print(Panel("[bold green]Conversion Tool[/bold green]"))
 
     options = {
-        "1": "Convertir APR en rewardsPerSharePerBlock",
-        "2": "Convertir rewardsPerSharePerBlock en APR",
-        "3": "Calculer les rewards sur une durée donnée et un APR donné avec un nombre de shares spécifié"
+        "1": "Convert APR to rewardsPerSharePerBlock",
+        "2": "Convert rewardsPerSharePerBlock to APR",
+        "3": "Calculate rewards over a given duration and APR for a specified USDC amount"
     }
 
     for key, value in options.items():
         print(f"[bold cyan]{key}:[/bold cyan] {value}")
 
-    choice = Prompt.ask("Entrez votre choix", choices=["1", "2", "3"])
+    choice = Prompt.ask("Enter your choice", choices=["1", "2", "3"])
 
     if choice == "1":
-        apr_value = float(Prompt.ask("Entrez l'APR (par exemple, pour 3%, entrez 0.03)"))
+        apr_value = float(Prompt.ask("Enter the APR (e.g., for 3%, input 0.03)"))
         print(f"rewardsPerSharePerBlock: [bold yellow]{apr_to_rewards_per_block(apr_value):.15f}[/bold yellow]")
 
     elif choice == "2":
-        rewards_value = float(Prompt.ask("Entrez le rewardsPerSharePerBlock"))
+        rewards_value = float(Prompt.ask("Enter the rewardsPerSharePerBlock"))
         print(f"APR: [bold yellow]{rewards_per_block_to_apr(rewards_value) * 100:.2f}%[/bold yellow]")
-
     elif choice == "3":
-        apr_value = float(Prompt.ask("Entrez l'APR (par exemple, pour 3%, entrez 0.03)"))
-        duration = float(Prompt.ask("Entrez la durée en jours"))
-        shares = float(Prompt.ask("Entrez le nombre de shares"))
-        print(f"Récompense totale pour [bold yellow]{shares}[/bold yellow] shares pendant [bold yellow]{duration}[/bold yellow] jours: [bold yellow]{calculate_rewards(duration, apr_value, shares):.15f}[/bold yellow]")
+        apr_value = float(Prompt.ask("Enter the APR (e.g., for 3%, input 0.03)"))
+        duration = float(Prompt.ask("Enter the duration in days"))
+        usdc_amount = float(Prompt.ask("Enter the amount in USDC"))
+        total_rewards_in_shares = calculate_rewards(duration, apr_value, usdc_amount)
+        total_rewards_in_usdc = total_rewards_in_shares / USDC_TO_SHARES
+        print(f"Total reward for [bold yellow]{usdc_amount}[/bold yellow] USDC (equivalent to [bold yellow]{usdc_amount * USDC_TO_SHARES}[/bold yellow] shares) over [bold yellow]{duration}[/bold yellow] days:")
+        print(f"Shares: [bold yellow]{total_rewards_in_shares:.15f}[/bold yellow]")
+        print(f"USDC: [bold yellow]{total_rewards_in_usdc:.6f}[/bold yellow]")
 
     else:
-        print("[bold red]Choix invalide![/bold red]")
+        print("[bold red]Invalid choice![/bold red]")
